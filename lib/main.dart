ebb3bbf831c164162db6836b0457f5cf3214aa2c
Dhananjay Sonar <sonardjay24.official@gmail.com>
2022-11-29 11:37:22 +0530

    Basics Completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_catalog/pages/home_page.dart';
 import 'package:flutter_catalog/pages/login_page.dart';
+import 'package:flutter_catalog/utils/routes.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 void main(){
@@ -19,12 +20,13 @@ class myApp extends StatelessWidget {
         fontFamily: GoogleFonts.lato().fontFamily,
         // primaryTextTheme: GoogleFonts.latoTextTheme(),
       ) ,
+      debugShowCheckedModeBanner: false ,
       darkTheme: ThemeData(
         brightness: Brightness.dark,
       ),
       routes: {
         "/":(context)=>HomePage(),
-        "/login": (context) => LoginPage(),
+        MyRoutes.loginRoute : (context) => LoginPage(),
       },
     );
   }
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,53 +1,110 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_catalog/pages/home_page.dart';
 
-class LoginPage extends StatelessWidget {
+import '../utils/routes.dart';
 
+class LoginPage extends StatefulWidget {
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  String name = "";
+  bool changeButton = false;
   @override
   Widget build(BuildContext context) {
     return Material(
       color: Colors.white,
-      child: Column(
-        children: [
-          Image.asset("images/login_image.png",fit:BoxFit.cover),
-          SizedBox(height:20),
-          Text("WELCOME",
-            style: TextStyle(
-                fontSize:24,
-                fontWeight: FontWeight.bold,
-            ),
+
+      child: SingleChildScrollView(
+          child:Column(
+            children: [
+              Image.asset("images/login_image.png",fit:BoxFit.cover),
+              SizedBox(height:20),
+              Text("WELCOME "+name,
+                style: TextStyle(
+                    fontSize:30,
+                    fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height:20),
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  vertical: 16,
+                  horizontal: 32
+                ),
+                child: Column(
+                   children:[
+                     TextFormField(
+                       decoration: InputDecoration(
+                         hintText:"Enter Username",
+                         labelText: "Username",
+                       ),
+                       onChanged: (value){
+                         name = value;
+                         setState(() {});
+                       },
+                     ),
+                     TextFormField(
+                       obscureText: true ,
+                       decoration: InputDecoration(
+                         hintText:"Enter Password",
+                         labelText: "Password",
+                       ),
+                     ),
+                     SizedBox(height:20),
+                     // ElevatedButton(onPressed: () {
+                     //   Navigator.push(
+                     //     context,
+                     //     MaterialPageRoute(builder: (context) => HomePage() ),
+                     //   );
+                     // }, child: Text("Login"),
+                     //   style: TextButton.styleFrom(
+                     //     minimumSize: Size(99, 40)
+                     //   )
+                     // )
+
+                     InkWell(
+                       onTap:() async {
+                         setState(() {
+                           changeButton = true;
+                         });
+                         await Future.delayed(Duration(seconds: 1));
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) => HomePage() ),
+                          );
+                        },
+                       child: AnimatedContainer(
+                         duration: Duration(seconds: 1),
+                         width: changeButton ? 50 :  150 ,
+                         height: 50,
+                         alignment: Alignment.center,
+                           child: changeButton ? Icon(Icons.done, color: Colors.white,) : Text(
+                           "Login",
+                           style: TextStyle(
+                             color: Colors.white,
+                             fontWeight: FontWeight.bold,
+                             fontSize: 18,
+                           ),
+                         ),
+                         decoration: BoxDecoration(
+                           color: Colors.deepPurple,
+                           borderRadius: BorderRadius.circular((
+                               changeButton?50:8
+                           )),
+                         ),
+                       ),
+                     )
+
+
+                   ]
+
+                ),
+              )
+            ],
           ),
-          SizedBox(height:20),
-          Padding(
-            padding: const EdgeInsets.symmetric(
-              vertical: 16,
-              horizontal: 32
-            ),
-            child: Column(
-               children:[
-                 TextFormField(
-                   decoration: InputDecoration(
-                     hintText:"Enter Username",
-                     labelText: "Username",
-                   ),),
-                 TextFormField(
-                   obscureText: true ,
-                   decoration: InputDecoration(
-                     hintText:"Enter Password",
-                     labelText: "Password",
-                   ),
-                 ),
-                 SizedBox(height:20),
-                 ElevatedButton(onPressed: ()=>{
-                   print("Button Clicked")
-                 }, child: Text("Login"),
-                   style: TextButton.styleFrom()
-
-                 )
-               ]
-
-            ),
-          )
-        ],
       ),
     ) ;
   }
